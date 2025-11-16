209b941712fd0c72fbc8cb0195b654ae0f8ece5b
Martin Novoa <martinnovoar2@gmail.com>
2025-11-16 18:06:45 -0300

    add incoming message types

---
+++ TP-Integrador/server/src/main/kotlin/IncomingMessages.kt
@@ -0,0 +1,81 @@
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import java.util.UUID
+
+sealed class WsMessage {
+    abstract val type: String
+}
+
+/**
+ * create_game
+ */
+
+data class CreateGameMessage(
+    override val type: String,
+) : WsMessage()
+
+/**
+ * join_game
+ */
+
+data class JoinGameMessage(
+    override val type: String,
+    val payload: JoinGamePayload
+) : WsMessage()
+
+data class JoinGamePayload(
+    val gameId: String
+) {
+    init {
+        require(isValidUUID(gameId)) { "gameId must be a valid UUID" }
+    }
+
+    private fun isValidUUID(s: String): Boolean =
+        runCatching { UUID.fromString(s) }.isSuccess
+}
+
+/**
+ * make_move
+ */
+
+data class MakeMoveMessage(
+    override val type: String,
+    val payload: MakeMovePayload
+) : WsMessage()
+
+data class MakeMovePayload(
+    val from: String,
+    val to: String
+) {
+    init {
+        require(isValidSquare(from)) { "Invalid from square: $from" }
+        require(isValidSquare(to)) { "Invalid to square: $to" }
+    }
+
+    private fun isValidSquare(s: String): Boolean =
+        s.length == 2 &&
+                s[0] in 'a'..'h' &&
+                s[1] in '1'..'8'
+}
+
+/**
+ * leave_game
+ */
+
+data class LeaveGameMessage(
+    override val type: String,
+) : WsMessage()
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type",
+    visible = true,
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(MakeMoveMessage::class, name = "make_move"),
+    JsonSubTypes.Type(CreateGameMessage::class, name = "create_game"),
+    JsonSubTypes.Type(JoinGameMessage::class, name = "join_game"),
+    JsonSubTypes.Type(LeaveGameMessage::class, name = "leave_game"),
+)
+abstract class WsMessageMixin
