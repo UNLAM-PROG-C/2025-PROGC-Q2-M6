493093711fa594791067d27b3f489e2496ad5a5d
Martin Novoa <martinnovoar2@gmail.com>
2025-11-19 20:53:10 -0300

    add last move logic to server

--- TP-Integrador/server/src/main/kotlin/DataClasses.kt
+++ TP-Integrador/server/src/main/kotlin/DataClasses.kt
@@ -1,10 +1,11 @@
+import com.github.bhlangonijr.chesslib.Piece
 import com.github.bhlangonijr.chesslib.Square
 import com.github.bhlangonijr.chesslib.move.Move
 import org.eclipse.jetty.websocket.api.Session
 
 class SimpleMove(val from: String, val to: String) {
-    fun toMove(): Move {
-        return Move(Square.valueOf(from), Square.valueOf(to))
+    fun toMove(promotion: Piece = Piece.NONE): Move {
+        return Move(Square.valueOf(from), Square.valueOf(to), promotion)
     }
 }
 data class Player(val id: String, val session: Session, val color: String)
