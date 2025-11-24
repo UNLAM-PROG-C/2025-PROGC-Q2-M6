36cd34e73da69efe0849753a1036ef5cabc97be7
Martin Novoa <martinnovoar2@gmail.com>
2025-11-24 00:07:56 -0300

    revert workflow version

--- TP-Integrador/server/build.gradle.kts
+++ TP-Integrador/server/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   kotlin("jvm") version "2.2.0"
   application
-  id("org.graalvm.buildtools.native") version "0.11.1"
+  id("org.graalvm.buildtools.native") version "0.10.4"
 }
 
 group = "com.example"
@@ -48,4 +48,5 @@ graalvmNative {
             buildArgs.add("--no-fallback")
         }
     }
+    toolchainDetection.set(false)
 }
