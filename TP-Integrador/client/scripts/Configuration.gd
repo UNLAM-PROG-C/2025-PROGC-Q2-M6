46c60e1d965f0ca9e20a4e839ac3efab70912872
Martin Novoa <martinnovoar2@gmail.com>
2025-11-24 22:35:30 -0300

    add configuration singleton

---
+++ TP-Integrador/client/scripts/Configuration.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+class TileColors:
+	const LIGHT := Color(0.9, 0.9, 0.9)
+	const DARK := Color(0.4, 0.3, 0.2)
+	const HIGHLIGHT := Color(1.0, 0.85, 0.2, 0.6)
+
+class MovementIndicatorConfig:
+	const DIAMETER_RATIO := 0.75
+	const LINE_WIDTH := 6
+	const COLOR := Color(0,0,0,0.6)
+	const POINTS := 128
+
+class PlayerColor:
+	const WHITE := "WHITE"
+	const BLACK := "BLACK"
